87d2a13a35dc21fdaf1d6fdcf8ab080b0cd31dc3
Eduardo Marvil <emtv2126@gmail.com>
2024-04-20 18:30:34 -0600

    Agregar archivos de proyecto.

---
+++ Webapp/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿namespace Webapp.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string NombreCompleto { get; set; }
+
+        public string Correo {  get; set; }
+
+        public DateOnly FechaInicio { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
